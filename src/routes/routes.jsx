cd0268374e178692a5bfbfe0175f8c1b0fd1b46c
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-30 12:32:05 -0300

    feat(routes): add route for About page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
+import About from "@pages/About";
 
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
     </Routes>
   );
 }
