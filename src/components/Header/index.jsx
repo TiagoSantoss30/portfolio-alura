897a4b91c5ff93eadc59e3254a1f00a450fdd04e
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-30 12:31:28 -0300

    style(header): update header component layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import * as S from "./styles.js";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <S.Header>
       <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/About"}>About</Link>
       </nav>
     </S.Header>
   );
